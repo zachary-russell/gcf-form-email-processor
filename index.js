b39dcdc20918d942b0c54a6d4c22bc20605475ba
Zach Russell <zach@protechig.com>
2017-10-04 15:54:13 -0400

    cleanup and refactor

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-var api_key = 'api-key-here';
-var sending_domain = 'sandbox3d603fef70dc4a3db61d4f9de9854c46.mailgun.org';
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: sending_domain});
+var api_key = 'your-api-key-here',
+    sending_domain = 'your-sending-domain-here',
+    mailgun = require('mailgun-js')({apiKey: api_key, domain: sending_domain});
 
 
 exports.processForm =  function processForm(req, res) {
-    query = req.query;
- var data = {
-    from: query.fname + ' ' + query.lname + ' <' + decodeURIComponent(query.email) + '>',
-    to: 'zach@protechig.com',
-    subject: 'New Submission From ' + query['fname'] + ' ' + query['lname'] ,
-    text: decodeURIComponent(query.message)
-}
-   console.log(query);
+    var query = req.query;
+        fname = decodeURIComponent(query['fname']),
+        lname = decodeURIComponent(query['lname']),
+        email = decodeURIComponent(query['email']),
+        message = decodeURIComponent(query['message']),
+        data = {
+            from: fname + ' ' + lname + ' <' + email + '>',
+            to: 'zach@protechig.com',
+            subject: 'New Submission From ' + fname + ' ' + lname ,
+            text: message
+        }
+
     mailgun.messages().send(data, function (error, body) {
-          res.send(body);
+        if (error) {
+            return res.send(error);
+        }
+        res.send(body);
 
     });
 };

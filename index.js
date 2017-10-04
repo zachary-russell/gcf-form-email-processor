39d9d351110856e45c2d3f36968ab46c66e1c809
Zach Russell <zach@protechig.com>
2017-10-04 12:22:58 -0400

    remove api key from git

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var api_key = 'key-3d9709a5f97486c6ce8bb09cc8f26d5c';
+var api_key = 'api-key-here';
 var sending_domain = 'sandbox3d603fef70dc4a3db61d4f9de9854c46.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: sending_domain});
 
